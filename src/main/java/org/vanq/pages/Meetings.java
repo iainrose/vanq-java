0b27f324282dd9a901a2429f12ab2f841c3f978f
Iain Rose <iain.rose@gmail.com>
2012-05-30 21:44:01 -0700

    - Added meetings page content tests

--- src/main/java/org/vanq/pages/Meetings.java
+++ src/main/java/org/vanq/pages/Meetings.java
@@ -1,6 +1,11 @@
 package org.vanq.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Meetings extends AbstractBasePage {
 
@@ -11,5 +16,46 @@ public class Meetings extends AbstractBasePage {
     @Override
     public String getExpectedPageName() {
         return "Past Meetings and Materials";
+    }
+
+    // Locators
+    By dateColumnLocator = By.cssSelector("td");
+    By meetingsTableLocator = By.cssSelector("table table table");
+    By presentationDetailsColumnLocator = By.cssSelector("td + td");
+    By presenterNameLocator = By.cssSelector("a");
+
+    public String getPresenterNameForDate(String date) {
+
+        // Create WebElement for the meetings table
+        WebElement meetingsTable = driver.findElement(meetingsTableLocator);
+
+        // Get a list of all rows in the meetings table
+        List<WebElement> tableRows = meetingsTable.findElements(By.tagName("tr"));
+
+        // Iterate through the table rows
+        for (WebElement tableRow : tableRows) {
+
+            // Check for table rows containing presentation details by counting the columns in the row
+            if (tableRow.findElements(By.tagName("td")).size() > 1) {
+
+                // Get the column containing the presentation date
+                WebElement dateColumn = tableRow.findElement(dateColumnLocator);
+
+                // Get the date on the current table row
+                String dateForRow = dateColumn.getText();
+
+                // Check for a match on date
+                if (dateForRow.equals(date)) {
+
+                    // Get presentation details column
+                    WebElement presentationDetailsColumn = tableRow.findElement(presentationDetailsColumnLocator);
+
+                    // Get presenter name
+                    return presentationDetailsColumn.findElement(presenterNameLocator).getText();
+
+                }
+            }
+        }
+        throw new NoSuchElementException("No presentation found for " + date);
     }
 }
--- src/test/java/org/vanq/tests/MeetingsPageTest.java
+++ src/test/java/org/vanq/tests/MeetingsPageTest.java
@@ -1,5 +1,6 @@
 package org.vanq.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.vanq.SiteMap;
 import org.vanq.pages.Meetings;
@@ -7,12 +8,66 @@ import org.vanq.pages.Meetings;
 public class MeetingsPageTest extends BaseTest {
 
     @Test(groups = {"content"})
-    public void april262012PresenterTest() {
+    public void may312012PresenterTest() {
+
+        // Test Data
+        String date = "May. 31, 2012";
+        String expectedPresenterName = "Iain Rose";
         
         // This test loads the meetings page directly so that navigational issues from the home page do not fail the test
         driver.get(BASE_URL + SiteMap.meetings);
         
         // Initialize the meetings page object
         Meetings meetings = new Meetings(driver);
+
+        // Get the presenter name for the 31 May 2012 meeting
+        String actualPresenterName = meetings.getPresenterNameForDate(date);
+
+        // Assert displayed presenter name is correct for given date
+        Assert.assertEquals(actualPresenterName, expectedPresenterName);
+
     }
+
+    @Test(groups = {"content"})
+    public void april262012PresenterTest() {
+
+        // Test Data
+        String date = "Apr. 26, 2012";
+        String expectedPresenterName = "Trevor Atkins";
+
+        // This test loads the meetings page directly so that navigational issues from the home page do not fail the test
+        driver.get(BASE_URL + SiteMap.meetings);
+
+        // Initialize the meetings page object
+        Meetings meetings = new Meetings(driver);
+
+        // Get the presenter name for the 26 April 2012 meeting
+        String actualPresenterName = meetings.getPresenterNameForDate(date);
+
+        // Assert displayed presenter name is correct for given date
+        Assert.assertEquals(actualPresenterName, expectedPresenterName);
+
+    }
+
+    @Test(groups = {"content"})
+    public void march292012PresenterTest() {
+
+        // Test Data
+        String date = "Mar. 29, 2012";
+        String expectedPresenterName = "Brent Cromarty";
+
+        // This test loads the meetings page directly so that navigational issues from the home page do not fail the test
+        driver.get(BASE_URL + SiteMap.meetings);
+
+        // Initialize the meetings page object
+        Meetings meetings = new Meetings(driver);
+
+        // Get the presenter name for the 29 March 2012 meeting
+        String actualPresenterName = meetings.getPresenterNameForDate(date);
+
+        // Assert displayed presenter name is correct for given date
+        Assert.assertEquals(actualPresenterName, expectedPresenterName);
+
+    }
+
 }
